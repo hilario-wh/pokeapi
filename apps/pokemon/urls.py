7c7157d23ccb7934ab4ed971ca7b65dc4db8b38e
hilario-wh <hilario@wisphub.net>
2022-02-11 10:11:12 -0500

    Agregadas url's y plantillas base

--- apps/pokemon/urls.py
+++ apps/pokemon/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import include, url
 
+from views import pokemon_list, pokemon_details, pokemon_type
+
 
 urlpatterns = [
+    url(r'^$', pokemon_list, name='list'),
+    url(r'^/details/(?P<id>\d+)/$', pokemon_details, name='list'),
+    url(r'^/type/(?P<id>\d+)/$', pokemon_type, name='list'),
 ]
--- apps/pokemon/views.py
+++ apps/pokemon/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def pokemon_list(request):
+    return render(request, template_name='pokemon/list.html')
+
+
+def pokemon_details(request, id):
+    return render(request, template_name='pokemon/details.html')
+
+
+def pokemon_type(request, id):
+    return render(request, template_name='pokemon/list.html')
